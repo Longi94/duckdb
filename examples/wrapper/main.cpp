b782d73c6710270cc1c96472e62db0fc3d828c61
Long Tran <lngtrn94@gmail.com>
2020-01-27 15:14:18 +0100

    read query from stdin

--- examples/wrapper/main.cpp
+++ examples/wrapper/main.cpp
@@ -1,13 +1,23 @@
 #include "duckdb.hpp"
+#include <iostream>
 
+using namespace std;
 using namespace duckdb;
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
+    if (argc < 2) {
         return 1;
     }
 	DuckDB db(argv[1]);
 	Connection con(db);
-	auto result = con.Query(argv[2]);
+    
+    string query;
+    string line;
+    
+    while (getline(cin, line)) {
+        query.append(line);
+    }
+    
+	auto result = con.Query(query);
     fprintf(stdout, "%s", result->ToString().c_str());
 }
